f7565171db027263e3e8e661777977263c1ede78
NicolasCastagnola <castagnola.nicolas@gmail.com>
2022-12-09 15:25:30 -0300

    more exte

--- Runtime/Extensions/Extensions.cs
+++ Runtime/Extensions/Extensions.cs
@@ -990,3 +990,71 @@ public static class Extensions
     }
 }
 
+public static class VectorExtensionMethods {
+
+	public static Vector2 xy(this Vector3 v) {
+		return new Vector2(v.x, v.y);
+	}
+
+	public static Vector3 WithX(this Vector3 v, float x) {
+		return new Vector3(x, v.y, v.z);
+	}
+
+	public static Vector3 WithY(this Vector3 v, float y) {
+		return new Vector3(v.x, y, v.z);
+	}
+
+	public static Vector3 WithZ(this Vector3 v, float z) {
+		return new Vector3(v.x, v.y, z);
+	}
+
+	public static Vector2 WithX(this Vector2 v, float x) {
+		return new Vector2(x, v.y);
+	}
+	
+	public static Vector2 WithY(this Vector2 v, float y) {
+		return new Vector2(v.x, y);
+	}
+	
+	public static Vector3 WithZ(this Vector2 v, float z) {
+		return new Vector3(v.x, v.y, z);
+        }
+        
+    // axisDirection - unit vector in direction of an axis (eg, defines a line that passes through zero)
+    // point - the point to find nearest on line for
+    public static Vector3 NearestPointOnAxis(this Vector3 axisDirection, Vector3 point, bool isNormalized = false)
+    {
+        if (!isNormalized) axisDirection.Normalize();
+        var d = Vector3.Dot(point, axisDirection);
+        return axisDirection * d;
+    }
+
+    // lineDirection - unit vector in direction of line
+    // pointOnLine - a point on the line (allowing us to define an actual line in space)
+    // point - the point to find nearest on line for
+    public static Vector3 NearestPointOnLine(
+        this Vector3 lineDirection, Vector3 point, Vector3 pointOnLine, bool isNormalized = false)
+    {
+        if (!isNormalized) lineDirection.Normalize();
+        var d = Vector3.Dot(point - pointOnLine, lineDirection);
+        return pointOnLine + (lineDirection * d);
+    }
+}
+
+public static class ExtensionMethods
+{
+
+    public static float LinearRemap(this float value,
+                                     float valueRangeMin, float valueRangeMax,
+                                     float newRangeMin, float newRangeMax)
+    {
+        return (value - valueRangeMin) / (valueRangeMax - valueRangeMin) * (newRangeMax - newRangeMin) + newRangeMin;
+    }
+
+    public static int WithRandomSign(this int value, float negativeProbability = 0.5f)
+    {
+        return Random.value < negativeProbability ? -value : value;
+    }
+
+}
+
